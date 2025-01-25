4f8eab6eafe4c5f6b48ea66ca542e3a18118f40e
MitsinjoFehizoro <mitsinjofehizoro@gmail.com>
2025-01-25 05:50:59 +0300

    Component card et themedText

---
+++ components/Card.tsx
@@ -0,0 +1,16 @@
+import { Shadows } from "@/constants/Shadows";
+import { useThemeColors } from "@/hooks/useThemeColors";
+import { StyleSheet, View, ViewProps } from "react-native";
+
+type Props = ViewProps
+
+export function Card({ style, ...rest }: Props) {
+	const colors = useThemeColors()
+	return <View style={[style = { backgroundColor: colors.grayWhite }, styles.container, style]} {...rest} />
+}
+
+const styles = StyleSheet.create({
+	container: {
+		borderRadius: 8, ...Shadows.dp2
+	}
+})
--- components/ThemedText.tsx
+++ components/ThemedText.tsx
@@ -1,60 +1,40 @@
-import { Text, type TextProps, StyleSheet } from 'react-native';
+import { Colors } from "@/constants/Colors"
+import { useThemeColors } from "@/hooks/useThemeColors"
+import { StyleSheet, Text, TextProps } from "react-native"
 
-import { useThemeColor } from '@/hooks/useThemeColor';
+type Props = TextProps & {
+	variant?: keyof typeof styles,
+	color?: keyof typeof Colors['light']
+}
 
-export type ThemedTextProps = TextProps & {
-  lightColor?: string;
-  darkColor?: string;
-  type?: 'default' | 'title' | 'defaultSemiBold' | 'subtitle' | 'link';
-};
-
-export function ThemedText({
-  style,
-  lightColor,
-  darkColor,
-  type = 'default',
-  ...rest
-}: ThemedTextProps) {
-  const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
-
-  return (
-    <Text
-      style={[
-        { color },
-        type === 'default' ? styles.default : undefined,
-        type === 'title' ? styles.title : undefined,
-        type === 'defaultSemiBold' ? styles.defaultSemiBold : undefined,
-        type === 'subtitle' ? styles.subtitle : undefined,
-        type === 'link' ? styles.link : undefined,
-        style,
-      ]}
-      {...rest}
-    />
-  );
+export function ThemedText({ variant, color, style, ...rest }: Props) {
+	const colors = useThemeColors()
+	return <Text style={[styles[variant ?? 'body3'], { color: colors[color ?? 'grayDark'] }, style]} {...rest} />
 }
 
 const styles = StyleSheet.create({
-  default: {
-    fontSize: 16,
-    lineHeight: 24,
-  },
-  defaultSemiBold: {
-    fontSize: 16,
-    lineHeight: 24,
-    fontWeight: '600',
-  },
-  title: {
-    fontSize: 32,
-    fontWeight: 'bold',
-    lineHeight: 32,
-  },
-  subtitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  link: {
-    lineHeight: 30,
-    fontSize: 16,
-    color: '#0a7ea4',
-  },
-});
+	headline: {
+		fontSize: 24, lineHeight: 32, fontWeight: 'bold'
+	},
+	subtitle1: {
+		fontSize: 14, lineHeight: 16, fontWeight: 'bold'
+	},
+	subtitle2: {
+		fontSize: 12, lineHeight: 16, fontWeight: 'bold'
+	},
+	subtitle3: {
+		fontSize: 10, lineHeight: 16, fontWeight: 'bold'
+	},
+	body1: {
+		fontSize: 14, lineHeight: 16, fontWeight: 'regular'
+	},
+	body2: {
+		fontSize: 12, lineHeight: 16, fontWeight: 'regular'
+	},
+	body3: {
+		fontSize: 10, lineHeight: 16, fontWeight: 'regular'
+	},
+	caption: {
+		fontSize: 8, lineHeight: 12, fontWeight: 'regular'
+	}
+})
